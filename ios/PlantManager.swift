e9c1cb8c989090a0f4b546bfc7c3ac3ccbb4c233
Roberta Samistraro Tomigian <ro.samis.comp@gmail.com>
2021-04-26 15:02:41 -0300

    first commit

---
+++ ios/PlantManager.swift
@@ -0,0 +1,8 @@
+//
+//  PlantManager.swift
+//  PlantManager
+//
+//  Created by Roberta Tomigian  on 26/04/21.
+//
+
+import Foundation
---
+++ src/pages/Confirmation.tsx
@@ -0,0 +1,74 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import {Button} from '../components/Button';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export const Confirmation = () => {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <Text style={styles.emoji}>{'😄'}</Text>
+        <Text style={styles.title}>Prontinho</Text>
+        <Text style={styles.subtitle}>
+          Agora vamos comecar a cuidar das suas plantinhas com muito cuidado.
+        </Text>
+        <View style={styles.footer}>
+          <Button title="Comecar" onPress={handleMoveOn} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    padding: 30,
+  },
+  emoji: {
+    fontSize: 78,
+  },
+  title: {
+    fontSize: 22,
+    lineHeight: 38,
+    textAlign: 'center',
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    textAlign: 'center',
+    fontSize: 17,
+    paddingVertical: 10,
+    color: colors.heading,
+  },
+  footer: {
+    width: '100%',
+    paddingHorizontal: 50,
+    marginTop: 20,
+  },
+});
